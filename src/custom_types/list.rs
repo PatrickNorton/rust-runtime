ce4c364dbc234c8feb3415a69ca3a7f4ee41c208
Patrick Norton <patrick.147.norton@gmail.com>
2020-04-27 08:28:04 -0400

    Added list

---
+++ src/custom_types/list.rs
@@ -0,0 +1,89 @@
+use crate::custom_var::CustomVar;
+use crate::method::{InnerMethod, StdMethod};
+use crate::operator::Operator;
+use crate::runtime::Runtime;
+use crate::std_type::Type;
+use crate::variable::{FnResult, Name, Variable};
+use num::ToPrimitive;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, Clone)]
+pub struct List {
+    value: Rc<RefCell<Vec<Variable>>>,
+}
+
+impl List {
+    pub fn from_values(values: Vec<Variable>) -> List {
+        List {
+            value: Rc::new(RefCell::new(values)),
+        }
+    }
+
+    fn get_operator(&self, name: Operator) -> Variable {
+        let value = match name {
+            Operator::Bool => List::list_bool,
+            Operator::Str => List::list_str,
+            Operator::GetAttr => List::list_index,
+            _ => unimplemented!(),
+        };
+        Variable::Method(Box::new(StdMethod::new(
+            self.clone(),
+            InnerMethod::Native(value),
+        )))
+    }
+
+    fn list_bool(&self, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+        debug_assert!(args.is_empty());
+        runtime.push(Variable::Bool(!self.value.borrow().is_empty()));
+        FnResult::Ok(())
+    }
+
+    fn list_str(&self, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+        debug_assert!(args.is_empty());
+        let mut value: String = String::new();
+        value += "[";
+        for arg in self.value.borrow().iter().enumerate() {
+            value += arg.1.str(runtime)?.as_str();
+            if arg.0 != self.value.borrow().len() - 1 {
+                value += ", ";
+            }
+        }
+        value += "]";
+        FnResult::Ok(())
+    }
+
+    fn list_index(&self, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+        debug_assert!(args.len() == 1);
+        let index = args[0].int(runtime)?;
+        if index > self.value.borrow().len().into() {
+            runtime.throw(Variable::String("index out of range".into()))
+        } else {
+            runtime.push(self.value.borrow()[index.to_usize().unwrap()].clone());
+            Result::Ok(())
+        }
+    }
+}
+
+impl CustomVar for List {
+    fn get_attr(&self, name: Name) -> Variable {
+        return match name {
+            Name::Operator(o) => self.get_operator(o),
+            Name::Attribute(_) => unimplemented!(),
+        };
+    }
+
+    fn set(&mut self, name: Name, object: Variable) {
+        unimplemented!()
+    }
+
+    fn get_type(&self) -> Type {
+        unimplemented!()
+    }
+}
+
+impl Into<Variable> for List {
+    fn into(self) -> Variable {
+        Box::new(self).into()
+    }
+}
--- src/custom_var.rs
+++ src/custom_var.rs
@@ -7,14 +7,32 @@ use num::BigInt;
 use std::fmt::Debug;
 use std::hash::{Hash, Hasher};
 use std::ops::Deref;
-use std::rc::Rc;
 
 #[derive(Debug, Clone)]
 pub struct CustomVarWrapper {
-    value: Rc<dyn CustomVar>,
+    value: Box<dyn CustomVar>,
 }
 
-pub trait CustomVar: Debug {
+pub trait CloneBox {
+    fn clone_box(&self) -> Box<dyn CustomVar>;
+}
+
+impl<T> CloneBox for T
+where
+    T: 'static + CustomVar + Clone,
+{
+    fn clone_box(&self) -> Box<dyn CustomVar> {
+        Box::new(self.clone())
+    }
+}
+
+impl Clone for Box<dyn CustomVar> {
+    fn clone(&self) -> Box<dyn CustomVar> {
+        self.clone_box()
+    }
+}
+
+pub trait CustomVar: Debug + CloneBox {
     fn get_attr(&self, name: Name) -> Variable;
     fn set(&mut self, name: Name, object: Variable);
     fn get_type(&self) -> Type;
@@ -50,13 +68,13 @@ pub trait CustomVar: Debug {
 }
 
 impl CustomVarWrapper {
-    pub fn new(value: Rc<dyn CustomVar>) -> CustomVarWrapper {
+    pub fn new(value: Box<dyn CustomVar>) -> CustomVarWrapper {
         CustomVarWrapper { value }
     }
 }
 
 impl Deref for CustomVarWrapper {
-    type Target = Rc<dyn CustomVar>;
+    type Target = Box<dyn CustomVar>;
 
     fn deref(&self) -> &Self::Target {
         &self.value
@@ -66,5 +84,14 @@ impl Deref for CustomVarWrapper {
 impl Hash for CustomVarWrapper {
     fn hash<H: Hasher>(&self, _state: &mut H) {
         unimplemented!()
+    }
+}
+
+impl<T: 'static> From<Box<T>> for Variable
+where
+    T: CustomVar,
+{
+    fn from(value: Box<T>) -> Self {
+        Variable::Custom(CustomVarWrapper::new(value))
     }
 }
