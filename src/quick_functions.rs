757f8b4bd9a5060d7fef47612527fbe83519a591
Patrick Norton <patrick.147.norton@gmail.com>
2020-05-15 10:26:19 -0400

    Changed quick_functions to throw exceptions instead of panic

--- src/quick_functions.rs
+++ src/quick_functions.rs
@@ -1,7 +1,8 @@
-use crate::custom_types::exceptions::index_error;
+use crate::custom_types::exceptions::{arithmetic_error, index_error};
 use crate::int_var::IntVar;
 use crate::operator::Operator;
 use crate::runtime::Runtime;
+use crate::string_var::StringVar;
 use crate::variable::{FromBool, Variable};
 use num::traits::Pow;
 use num::{BigRational, One, ToPrimitive, Zero};
@@ -96,9 +97,11 @@ pub fn quick_mul(this: Variable, other: Variable, runtime: &mut Runtime) -> Quic
         })),
         Variable::Bigint(i) => Result::Ok(Variable::Bigint(i * IntVar::from(other))),
         Variable::String(s) => {
-            let result = IntVar::from(other)
-                .to_usize()
-                .expect("Too many repetitions");
+            let big_var = IntVar::from(other);
+            let result = match big_var.to_usize() {
+                Option::Some(val) => val,
+                Option::None => return mul_err(big_var, runtime),
+            };
             Result::Ok(Variable::String(s.repeat(result).into()))
         }
         Variable::Decimal(d1) => {
@@ -118,6 +121,20 @@ pub fn quick_mul(this: Variable, other: Variable, runtime: &mut Runtime) -> Quic
         Variable::Function(_) => unimplemented!(),
         Variable::Custom(_) => unimplemented!(),
     }
+}
+
+fn mul_err(big_var: IntVar, runtime: &mut Runtime) -> QuickResult {
+    runtime.throw_quick(
+        arithmetic_error(),
+        format!(
+            "Too many string repetitions: max number of shifts \
+                for a non-empty string is {}, attempted to shift by {}",
+            usize::MAX,
+            big_var,
+        )
+        .into(),
+    )?;
+    unreachable!()
 }
 
 pub fn quick_div(this: Variable, other: Variable, runtime: &mut Runtime) -> QuickResult {
@@ -264,18 +281,8 @@ pub fn quick_power(this: Variable, other: Variable, runtime: &mut Runtime) -> Qu
 pub fn quick_left_bitshift(this: Variable, other: Variable, runtime: &mut Runtime) -> QuickResult {
     match this {
         Variable::Null() => unimplemented!(),
-        Variable::Bool(b) => Result::Ok(Variable::Bigint(
-            (if b { 1 } else { 0 }
-                << IntVar::from(other)
-                    .to_usize()
-                    .expect("Value too big to shift"))
-            .into(),
-        )),
-        Variable::Bigint(i) => Result::Ok(Variable::Bigint(
-            i << IntVar::from(other)
-                .to_usize()
-                .expect("Value too big to shift"),
-        )),
+        Variable::Bool(b) => q_lshift(IntVar::from_bool(b), other.into(), runtime),
+        Variable::Bigint(i) => q_lshift(i, other.into(), runtime),
         Variable::String(_) => unimplemented!(),
         Variable::Decimal(_) => unimplemented!(),
         Variable::Char(_) => unimplemented!(),
@@ -290,21 +297,17 @@ pub fn quick_left_bitshift(this: Variable, other: Variable, runtime: &mut Runtim
     }
 }
 
+fn q_lshift(this: IntVar, other: IntVar, runtime: &mut Runtime) -> QuickResult {
+    let big_var = IntVar::from(other);
+    let other_usize = shift_to_usize(big_var, runtime)?;
+    Result::Ok(Variable::Bigint((this << other_usize).into()))
+}
+
 pub fn quick_right_bitshift(this: Variable, other: Variable, runtime: &mut Runtime) -> QuickResult {
     match this {
         Variable::Null() => unimplemented!(),
-        Variable::Bool(b) => Result::Ok(Variable::Bigint(
-            (if b { 1 } else { 0 }
-                >> IntVar::from(other)
-                    .to_usize()
-                    .expect("Value too big to shift"))
-            .into(),
-        )),
-        Variable::Bigint(i) => Result::Ok(Variable::Bigint(
-            i >> IntVar::from(other)
-                .to_usize()
-                .expect("Value too big to shift"),
-        )),
+        Variable::Bool(b) => q_rshift(IntVar::from_bool(b), other.into(), runtime),
+        Variable::Bigint(i) => q_rshift(i, other.into(), runtime),
         Variable::String(_) => unimplemented!(),
         Variable::Decimal(_) => unimplemented!(),
         Variable::Char(_) => unimplemented!(),
@@ -317,6 +320,31 @@ pub fn quick_right_bitshift(this: Variable, other: Variable, runtime: &mut Runti
         Variable::Function(_) => unimplemented!(),
         Variable::Custom(_) => unimplemented!(),
     }
+}
+
+fn q_rshift(this: IntVar, other: IntVar, runtime: &mut Runtime) -> QuickResult {
+    let big_var = IntVar::from(other);
+    let other_usize = shift_to_usize(big_var, runtime)?;
+    Result::Ok(Variable::Bigint((this >> other_usize).into()))
+}
+
+fn shift_to_usize(big_var: IntVar, runtime: &mut Runtime) -> Result<usize, ()> {
+    Result::Ok(match big_var.to_usize() {
+        Option::Some(val) => val,
+        Option::None => {
+            runtime.throw_quick(arithmetic_error(), shift_err(big_var))?;
+            unreachable!()
+        }
+    })
+}
+
+fn shift_err(big_val: IntVar) -> StringVar {
+    format!(
+        "Attempted bitshift of {}, which is more than the max allowed shift {}",
+        big_val,
+        usize::MAX
+    )
+    .into()
 }
 
 pub fn quick_bitwise_and(this: Variable, other: Variable, runtime: &mut Runtime) -> QuickResult {
