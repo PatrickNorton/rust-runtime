cf623c42e7997f08619f0779a74073c2dd1b257b
Patrick Norton <patrick.147.norton@gmail.com>
2021-01-14 10:49:37 -0500

    Removed unnecessary clone

--- src/custom_types/dict.rs
+++ src/custom_types/dict.rs
@@ -545,7 +545,7 @@ impl DictIter {
 
     fn next_fn(self: Rc<Self>, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
         debug_assert!(args.is_empty());
-        match self.clone().true_next(runtime)? {
+        match self.true_next(runtime)? {
             Option::None => runtime.return_n(vec![Option::None.into(), Option::None.into()]),
             Option::Some(val) => {
                 runtime.return_n(vec![Option::Some(val.0).into(), Option::Some(val.1).into()])
