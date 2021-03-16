d4d66fc27a57a57badd0a4726f8a03c9405c298f
Patrick Norton <patrick.147.norton@gmail.com>
2021-03-16 07:04:46 -0400

    Added use of raw strings

--- src/character.rs
+++ src/character.rs
@@ -2,21 +2,21 @@ use std::borrow::Cow;
 
 pub fn repr(value: char) -> Cow<'static, str> {
     match value {
-        '\\' => "\\\\".into(),
-        '"' => "\\\"".into(),
-        '\0' => "\\0".into(),
-        '\x07' => "\\a".into(),
-        '\x08' => "\\b".into(),
-        '\x0C' => "\\f".into(),
-        '\n' => "\\n".into(),
-        '\r' => "\\r".into(),
-        '\t' => "\\t".into(),
-        '\x0B' => "\\v".into(),
+        '\\' => r"\\".into(),
+        '"' => r#"\""#.into(),
+        '\0' => r"\0".into(),
+        '\x07' => r"\a".into(),
+        '\x08' => r"\b".into(),
+        '\x0C' => r"\f".into(),
+        '\n' => r"\n".into(),
+        '\r' => r"\r".into(),
+        '\t' => r"\t".into(),
+        '\x0B' => r"\v".into(),
         x if x.is_ascii_graphic() || x == ' ' => x.to_string().into(),
-        x if x.is_ascii() => format!("\\x{:02X}", x as u32).into(),
+        x if x.is_ascii() => format!(r"\x{:02X}", x as u32).into(),
         x => {
             let escaped = value.escape_debug().to_string();
-            if !escaped.starts_with("\\u") {
+            if !escaped.starts_with(r"\u") {
                 escaped.into()
             } else {
                 match x as u32 {
@@ -24,8 +24,8 @@ pub fn repr(value: char) -> Cow<'static, str> {
                         "ASCII characters should already be filtered out (got {:?})",
                         x
                     ),
-                    x @ 0x100..=0xFFFF => format!("\\u{:04X}", x).into(),
-                    x => format!("\\U{:08X}", x).into(),
+                    x @ 0x100..=0xFFFF => format!(r"\u{:04X}", x).into(),
+                    x => format!(r"\U{:08X}", x).into(),
                 }
             }
         }
