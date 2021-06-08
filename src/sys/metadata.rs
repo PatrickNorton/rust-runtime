a441e41265bb7947af3d6202cd0cf6b320d8fe7e
Patrick Norton <patrick.147.norton@gmail.com>
2021-06-08 06:35:21 -0400

    Added more metadata information

--- src/sys/metadata.rs
+++ src/sys/metadata.rs
@@ -7,7 +7,7 @@ use crate::runtime::Runtime;
 use crate::std_type::Type;
 use crate::sys::os_do_1;
 use crate::variable::{FnResult, Variable};
-use std::fs::Metadata;
+use std::fs::{FileType, Metadata, Permissions};
 use std::rc::Rc;
 
 pub fn metadata(args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
@@ -21,6 +21,16 @@ struct LangMetadata {
     value: Metadata,
 }
 
+#[derive(Debug)]
+struct LangFileType {
+    value: FileType,
+}
+
+#[derive(Debug)]
+struct LangPermissions {
+    value: Permissions,
+}
+
 impl LangMetadata {
     fn new(value: Metadata) -> Rc<LangMetadata> {
         Rc::new(LangMetadata { value })
@@ -29,6 +39,28 @@ impl LangMetadata {
     fn repr(self: Rc<Self>, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
         debug_assert!(args.is_empty());
         runtime.return_1(format!("{:?}", self.value).into())
+    }
+}
+
+impl LangFileType {
+    fn new(value: FileType) -> Rc<LangFileType> {
+        Rc::new(LangFileType { value })
+    }
+}
+
+impl LangPermissions {
+    fn new(value: Permissions) -> Rc<LangPermissions> {
+        Rc::new(LangPermissions { value })
+    }
+
+    fn mode(&self) -> Option<u32> {
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            Option::Some(self.value.mode())
+        }
+        #[cfg(not(unix))]
+        Option::None
     }
 }
 
@@ -53,7 +85,54 @@ impl CustomVar for LangMetadata {
             "isDir" => self.value.is_dir().into(),
             "isFile" => self.value.is_file().into(),
             "length" => self.value.len().into(),
+            "fileType" => LangFileType::new(self.value.file_type()).into(),
+            "permissions" => LangPermissions::new(self.value.permissions()).into(),
             x => unimplemented!("Metadata.{}", x),
+        }
+    }
+}
+
+impl CustomVar for LangFileType {
+    fn set(self: Rc<Self>, name: Name, object: Variable) {
+        unimplemented!()
+    }
+
+    fn get_type(&self) -> Type {
+        todo!()
+    }
+
+    fn get_operator(self: Rc<Self>, op: Operator) -> Variable {
+        todo!()
+    }
+
+    fn get_attribute(self: Rc<Self>, name: &str) -> Variable {
+        match name {
+            "isDir" => self.value.is_dir().into(),
+            "isFile" => self.value.is_file().into(),
+            "isSymlink" => self.value.is_symlink().into(),
+            x => unimplemented!("FileType.{}", x),
+        }
+    }
+}
+
+impl CustomVar for LangPermissions {
+    fn set(self: Rc<Self>, name: Name, object: Variable) {
+        unimplemented!()
+    }
+
+    fn get_type(&self) -> Type {
+        unimplemented!()
+    }
+
+    fn get_operator(self: Rc<Self>, op: Operator) -> Variable {
+        unimplemented!()
+    }
+
+    fn get_attribute(self: Rc<Self>, name: &str) -> Variable {
+        match name {
+            "readOnly" => self.value.readonly().into(),
+            "mode" => self.mode().map(From::from).into(),
+            x => unimplemented!("Permissions.{}", x),
         }
     }
 }
