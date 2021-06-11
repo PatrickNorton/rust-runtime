41e2e58c442e281974734ea2b3d7b09449bdff31
Patrick Norton <patrick.147.norton@gmail.com>
2021-06-10 21:00:36 -0400

    Added new character methods

--- src/builtin_functions/char_fn.rs
+++ src/builtin_functions/char_fn.rs
@@ -1,10 +1,13 @@
 use crate::character;
+use crate::custom_types::bytes::LangBytes;
 use crate::method::{NativeMethod, StdMethod};
 use crate::operator::Operator;
 use crate::runtime::Runtime;
 use crate::string_var::StringVar;
 use crate::variable::{FnResult, Variable};
 use ascii::{AsciiString, ToAsciiChar};
+use std::array::IntoIter;
+use std::rc::Rc;
 
 pub fn op_fn(o: Operator) -> NativeMethod<char> {
     match o {
@@ -25,6 +28,12 @@ pub fn attr_fn(s: &str) -> NativeMethod<char> {
     match s {
         "upper" => upper,
         "lower" => lower,
+        "isUpper" => is_upper,
+        "isLower" => is_lower,
+        "utf8Len" => utf8_len,
+        "utf16Len" => utf16_len,
+        "encodeUtf8" => encode_utf8,
+        "encodeUtf16" => encode_utf16,
         x => unimplemented!("char.{}", x),
     }
 }
@@ -56,6 +65,7 @@ fn repr(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
     debug_assert!(args.is_empty());
     let result: StringVar = match this {
         '\'' => "c\"'\"".into(),
+        '"' => "c'\"'".into(),
         x => character::repr(x).into(),
     };
     runtime.return_1(result.into())
@@ -69,4 +79,46 @@ fn upper(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
 fn lower(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
     debug_assert!(args.is_empty());
     runtime.return_1(this.to_lowercase().next().unwrap_or(this).into())
+}
+
+fn is_upper(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+    debug_assert!(args.is_empty());
+    runtime.return_1(this.is_uppercase().into())
+}
+
+fn is_lower(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+    debug_assert!(args.is_empty());
+    runtime.return_1(this.is_lowercase().into())
+}
+
+fn utf8_len(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+    debug_assert!(args.is_empty());
+    runtime.return_1(this.len_utf8().into())
+}
+
+fn utf16_len(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+    debug_assert!(args.is_empty());
+    runtime.return_1(this.len_utf16().into())
+}
+
+fn encode_utf8(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+    debug_assert!(args.is_empty());
+    let mut result = [0; 4];
+    let len = this.encode_utf8(&mut result).len();
+    runtime.return_1(Rc::new(LangBytes::new(result[..len].into())).into())
+}
+
+fn encode_utf16(this: char, args: Vec<Variable>, runtime: &mut Runtime) -> FnResult {
+    debug_assert!(args.is_empty());
+    let mut result = [0; 2];
+    let len = this.encode_utf16(&mut result).len();
+    runtime.return_1(
+        Rc::new(LangBytes::new(
+            result[..len]
+                .iter()
+                .flat_map(|x| IntoIter::new(x.to_le_bytes()))
+                .collect(),
+        ))
+        .into(),
+    )
 }
